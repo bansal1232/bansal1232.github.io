a9879267b0c1bec70617821c738a627a2e61e16d
Shubham <shubham@Shubham.local>
2026-04-23 23:38:42 +0530

    Add Google Analytics integration and update HTML files to include analytics script

---
+++ js/analytics.js
@@ -0,0 +1,14 @@
+(function () {
+  var GA_ID = 'G-0BN2CH6GH8';
+
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+  document.head.appendChild(s);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { window.dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', GA_ID);
+})();
