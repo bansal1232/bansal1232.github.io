b2eb460e90fe4d2cc6e47e65e11709b99e55763b
Shubham Bansal <shubhambansal1232@gmail.com>
2026-07-03 13:30:03 +0530

    google ads added

---
+++ js/ads.js
@@ -0,0 +1,10 @@
+(function () {
+  // Google AdSense — publisher ID
+  var CLIENT = 'ca-pub-4674703879068558';
+
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + CLIENT;
+  s.crossOrigin = 'anonymous';
+  document.head.appendChild(s);
+})();
